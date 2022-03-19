0b35e650058568d84df26de29bfcc3ecdb501188
susheng <2771885247@qq.com>
2022-03-20 00:14:46 +0800

    config: baseUrl

--- docs/config.js
+++ docs/config.js
@@ -8,6 +8,7 @@ module.exports = {
     theme: '@vuepress/theme-default',
     themeConfig: {
       logo: 'https://vuejs.org/images/logo.png',
+      base: '.',
       sidebar: [
         {
           text: '瞎几把记录',
